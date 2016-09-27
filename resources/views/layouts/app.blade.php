5cba98e20bf758768a73ff2261fe85688c9329e4
Piotr <szpak@op.pl>
2016-09-27 21:23:54 +0100

    fixed css error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 	<title>BaluSteel</title>
 	
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="/css/all.min.css" rel="stylesheet">
+    <link href="/css/all.min.css?version=1.0" rel="stylesheet">
 
 </head>
 <body>
